766dde5450fb89ed7f5bbb7839c84406039b0acf
sheena benavente <13785060+SheenaBenavente@users.noreply.github.com>
2023-03-06 19:05:33 -0500

    header and footer updates

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const headerMenu = document.querySelector('#desktop__nav');
+
+
+
+
+
+// EXAMPLE////////////////
+
+// const mobileMenuIcon = document.querySelector('#mobile-menu-icon');
+// const mobileMenuList = document.querySelector('#mobile-menu-list');
+// const mobileExitMenuIcon = document.querySelector('#mobile-exit-menu-icon');
+
+// const topicsMenuListIcon = document.querySelector('#topics-menu-open');
+// const topicsMenuList = document.querySelector('#topics-menu-list')
+// const topicsExitMenuIcon = document.querySelector('#topics-menu-close');
+
+// // Clicking on the hamburger menu drops down the main menu
+// mobileMenuIcon.addEventListener('click', function() {
+//   // 1. Unhide menu links/list
+//   mobileMenuList.classList.remove('hide');
+
+//   // 2. Hide hamburger menu icon
+//   mobileMenuIcon.classList.add('hide');
+
+//   // 3. Show exit menu icon
+//   mobileExitMenuIcon.classList.remove('hide');
+// });
+
+// // Clicking on the exit menu restores the menu back to default
+// mobileExitMenuIcon.addEventListener('click', function() {
+//   // 1. Hide menu links/list
+//   mobileMenuList.classList.add('hide');
+
+//   // 2. Show hamburger menu icon
+//   mobileMenuIcon.classList.remove('hide');
+
+//   // 3. Hide the exit menu icon
+//   mobileExitMenuIcon.classList.add('hide');
+// });
